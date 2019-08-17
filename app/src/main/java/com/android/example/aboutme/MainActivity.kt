1beb162197016ad8daf3830c67a87979dc6b0965
Anuruk Phoarun <cratyzero@gmail.com>
2019-08-17 23:09:18 +0700

    Add Author

--- app/src/main/java/com/android/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/android/example/aboutme/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
+
 
     }
 
